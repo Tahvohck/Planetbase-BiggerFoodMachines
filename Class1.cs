3272fd33ae1820b6443962f970eec5e0eb949e40
Tahvohck <Tahvohck@gmail.com>
2020-05-29 17:54:56 -0400

    Initial setup

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using Planetbase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+
+namespace Tahvohck_Mods
+{
+    public class BFM_Main : IMod
+    {
+        public void Init()
+        {
+            TahvUtil.Log("Initialized.");
+            ZZZ_Modhooker.PreResetEvent += Setup;
+#if DEBUG
+            ZZZ_Modhooker.PostResetEvent += Runcheck;
+#endif
+        }
+
+        public void Update() { }
+
+        public void Setup(object sender, EventArgs e)
+        {
+            new Harmony(typeof(BFM_Main).FullName).PatchAll();
+            TahvUtil.Log("Patched.");
+        }
+
+        public void Runcheck(object sender, EventArgs evArgs)
+        {
+            MealMaker mm = (MealMaker)TypeList<ComponentType, ComponentTypeList>.find<MealMaker>();
+            TahvUtil.Log($"MealMaker slots: {mm.getEmbeddedResourceCount()}");
+        } 
+    }
+
+
+    [HarmonyPatch(typeof(MealMaker), MethodType.Constructor)]
+    public class Patches
+    {
+        [HarmonyPostfix]
+        public static void MoreMealMakerSlots(MealMaker __instance)
+        {
+            __instance.mEmbeddedResourceCount = 12;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BiggerFoodMachines")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BiggerFoodMachines")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("76f95653-396b-4b98-aa52-aaf3c7dd7bdf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
